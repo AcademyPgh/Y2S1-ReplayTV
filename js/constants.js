c2ad05b67c8989a38371511d8ba265277552b75d
Jean Lange <langeje@us.ibm.com>
2017-07-26 17:47:05 -0400

    set consts as John says

--- js/constants.js
+++ js/constants.js
@@ -1,19 +1,19 @@
 // these are constants to control behavior in the application. Include this file and declare consts here; humans will edit it to make stuff change how it behaves.
 
 // eventing constants
-const tooFarPastScheduleTime        = 120000;
+const tooFarPastScheduleTime        = 120000; // 2 minutes
 const eventScheduleURL              = '/test_schedule.json?';
 const checkEventsIntervalInMS       = 5000;
 const fetchEventsIntervalInMS       = 10000;
-const displayAnnouncementThisManyMS = 3000;
+const displayAnnouncementThisManyMS = 15000;
 const announcementFadeInOutTimeInMS = 1000;
 
 // twitter
 const mongoURL                  = "https://api.mongolab.com/api/1/databases/replaytweets/collections/fxtweets";
 const mongoKEY                  = "GNTzkTa9ucksY89SsIgyW3rN0mdfc1AH";
-const tweetRotationIntervalInMS = 4500;
+const tweetRotationIntervalInMS = 15000;
 
 // other
-const appReloadIntervalInMS = 300000;
+const appReloadIntervalInMS = 7200000; // 2 hours
 const replayScheduleURL     = "http://replayfxcalendar.azurewebsites.net/";
-const bgChangeIntervalInMS  = 5000;
+const bgChangeIntervalInMS  = 30000;
